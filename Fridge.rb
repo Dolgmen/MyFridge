6d898da94b0a3e5bc3c5ce61fa96be377b9511f5
Dolgmen <slotsy046@gmail.com>
2021-05-24 14:38:25 +0300

    Develop

---
+++ Fridge.rb
@@ -0,0 +1,70 @@
+require 'json'
+require './file'
+
+class Fridge
+  attr_accessor :ingredients
+
+  def initialize
+    @file_name = 'fridge.json'
+  end
+
+  def show_ingredients
+    puts @ingredients
+  end
+
+  def append(name, quantity)
+    if @ingredients.has_key?(name)
+      @ingredients[name] += quantity
+    elsif @ingredients[name] = quantity
+    end
+  end
+
+  def to_json
+    p @ingredients
+    @ingredients.to_json
+  end
+
+  def save
+    text = to_json
+    MyFile::File_.write(@file_name, text)
+  end
+  def load
+    begin
+      @ingredients = JSON.parse(MyFile::File_.read(@file_name))
+    rescue
+      @ingredients = {}
+    end
+  end
+  def self.menu
+    while true
+
+      puts '1. Add ingredient into a fridge'
+      puts '2. Show ingredients into FRIDGE'
+      puts ''
+      puts '0. Back'
+
+      puts 'Enter a number '
+      user_input = gets.chomp
+      case user_input
+      when '0'
+        $FRIDGE.save
+        Menu.main
+      when '1'
+        add_ingredient
+      when '2'
+        $FRIDGE.show_ingredients
+      end
+    end
+  end
+
+end
+def add_ingredient
+  puts 'Enter a name of ingredient'
+  name_input = gets.chomp
+  puts 'Enter a quantity of ingredient'
+  quantity_input = gets.chomp.to_f
+  $FRIDGE.append(name_input, quantity_input)
+end
+$FRIDGE = Fridge.new
+$FRIDGE.load
+
---
+++ Search.rb
@@ -0,0 +1,60 @@
+
+load './Recipe.rb'
+require './Fridge.rb'
+
+
+class Search
+  def self.menu
+    while true
+      puts '1. Search by friedge'
+      puts '2. Search by ingredients'
+      puts '0. Back'
+      puts ''
+      puts 'Enter a number '
+      user_input = gets.chomp
+      case user_input
+      when '0'
+        Menu.main
+      when '1'
+        Search.by($FRIDGE.ingredients.keys)
+        puts ''
+        puts 'Puts Needed Recipe ID:'
+        recipe_id_input = gets.chomp
+        RecipeAct.with_recipe(recipe_id_input)
+      when '2'
+        Search.by(Search.ingredients_input)
+        puts ''
+        puts 'Puts Needed Recipe ID:'
+        recipe_id_input = gets.chomp
+        RecipeAct.with_recipe(recipe_id_input)
+      end
+    end
+  end
+
+  def self.ingredients_input
+    puts('Set a ingredients, using comma betweуn')
+    user_ingredients = gets.chomp
+    user_ingredients = user_ingredients.split(/,/)
+  end
+
+  def self.name_input
+    puts('Set a name')
+    recipe_name = gets.chomp
+  end
+
+  def self.by(user_ing_list)
+    recipe_data = DATA.all_recipes
+    searched_recipe = recipe_data.find_all { |recipe| recipe.ingredients & user_ing_list != [] }
+    searched_recipe.each { |recipe| puts "Recipe Name: #{recipe.name}, ID:#{recipe.id}" }
+  end
+
+  def self.by_name
+    recipe_data = DATA.all_recipes
+    recipe_name = name_input
+    searched_recipe = recipe_data.find_all { |recipe| recipe.name == recipe_name }
+    searched_recipe.each { |recipe| puts "Recipe Name: #{recipe.name}, ID:#{recipe.id}" }
+  end
+end
+
+
+
